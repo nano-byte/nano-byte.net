2620ed28eac2e4715bb7bc100f56a9096360c780
Bastian Eicher <bastian@eicher.net>
2020-08-16 17:19:07 +0200

    Added Matomo

--- site.js
+++ site.js
@@ -21,6 +21,8 @@ $(function () {
 
   navbarShrink();
   $(window).scroll(navbarShrink);
+
+  matomo();
 });
 
 function navbarShrink() {
@@ -30,3 +32,16 @@ function navbarShrink() {
     $(".navbar").removeClass("navbar-shrink");
   }
 };
+
+function matomo() {
+  var _paq = window._paq = window._paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function () {
+    var u = "https://matomo.nano-byte.net/";
+    _paq.push(['setTrackerUrl', u + 'matomo.php']);
+    _paq.push(['setSiteId', '2']);
+    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+    g.type = 'text/javascript'; g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
+  })();
+}
